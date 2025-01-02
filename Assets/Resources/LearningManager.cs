5395d4f019a178f21565655a39863fff1e7343b9
LemonkaPix <maciejwrobel029@gmail.com>
2025-01-02 20:16:26 +0100

    BFS alghoritm implementation

--- Assets/Resources/LearningManager.cs
+++ Assets/Resources/LearningManager.cs
@@ -11,8 +11,9 @@ public class LearningManager : MonoBehaviour
         foreach (float[] image in MNISTLoader.instance.trainingData)
         {
             bool[,] map = FloatArrayToBoolArray(image);
-        }
 
+            
+        }
     }
 
     private static bool[,] FloatArrayToBoolArray(float[] image)
@@ -37,10 +38,51 @@ public class LearningManager : MonoBehaviour
         return map;
     }
 
+    public static IEnumerator BFS(bool[,] grid, int startX, int startY, List<(int, int)> directions)
+    {
+        int rows = 28;
+        int cols = 28;
+
+        if (startX < 0 || startX >= rows || startY < 0 || startY >= cols || grid[startX, startY])
+        {
+            yield break;
+        }
+
+        int count = 0;
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+        queue.Enqueue((startX, startY));
+        grid[startX, startY] = true; // Mark as visited
+
+        while (queue.Count > 0)
+        {
+            var (x, y) = queue.Dequeue();
+            count++;
+
+            foreach (var (dx, dy) in directions)
+            {
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && !grid[nx, ny])
+                {
+                    queue.Enqueue((nx, ny));
+                    grid[nx, ny] = true; // Mark as visited
+                }
+            }
+            yield return new WaitForSeconds(.03f);
+            //print("TICK");
+            MNISTVisualizer.Instance.VisualizeImage(grid);
+        }
+
+        print(count);
+    }
+
     [Button]
     public void test()
     {
         var map = FloatArrayToBoolArray(MNISTLoader.instance.trainingData[0]);
         MNISTVisualizer.Instance.VisualizeImage(map);
+        StartCoroutine(BFS(map, 0, 27, new List<(int, int)> { (1, 0), (0, -1), (0, 1) }));
+
     }
 }
